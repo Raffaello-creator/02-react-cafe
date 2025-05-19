7bed47c5f0805f29490f581820810c4e604752b0
Raffaello-creator <wolf201026@gmail.com>
2025-05-19 16:34:41 +0200

    fix Votestats and VoteOptions

--- src/components/VoteStats/VoteStats.tsx
+++ src/components/VoteStats/VoteStats.tsx
@@ -1,15 +1,15 @@
 import css from "./VoteStats.module.css";
+import type { VoteType } from "../../types/votes.ts";
 
-interface VoteStatusProp {
-  votes: {
-    good: number;
-    neutral: number;
-    bad: number;
-  };
+type Votes = Record<VoteType, number>;
+
+interface VoteStatsProps {
+  votes: Votes;
   totalVotes: number;
   positiveRate: number;
 }
-const VoteStatus: React.FC<VoteStatusProp> = ({
+
+const VoteStats: React.FC<VoteStatsProps> = ({
   votes,
   totalVotes,
   positiveRate,
@@ -17,22 +17,22 @@ const VoteStatus: React.FC<VoteStatusProp> = ({
   return (
     <div className={css.container}>
       <p className={css.stat}>
-        Good:<strong>{votes.good}</strong>
+        Good: <strong>{votes.good}</strong>
       </p>
       <p className={css.stat}>
-        Neutral:<strong>{votes.neutral}</strong>
+        Neutral: <strong>{votes.neutral}</strong>
       </p>
       <p className={css.stat}>
-        Bad:<strong>{votes.bad}</strong>
+        Bad: <strong>{votes.bad}</strong>
       </p>
       <p className={css.stat}>
-        Total:<strong>{totalVotes}</strong>
+        Total: <strong>{totalVotes}</strong>
       </p>
       <p className={css.stat}>
-        Positive:<strong>{positiveRate}%</strong>
+        Positive: <strong>{positiveRate}%</strong>
       </p>
     </div>
   );
 };
 
-export default VoteStatus;
+export default VoteStats;
